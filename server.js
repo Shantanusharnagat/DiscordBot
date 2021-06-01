2e6141c5cdb84ec2f837a928d0f030fe5a8d5574
Glitch (cactus-careful-soy) <none>
2021-06-01 01:11:42 +0000

    💏💫 Checkpoint ./server.js:1062301/1216 ./src/pages/index.hbs:1062301/14 ./src/pages/admin.hbs:1062301/26

--- server.js
+++ server.js
@@ -41,42 +41,18 @@ fastify.get("/", async function (request, reply) {
   // params is an object we'll pass to our handlebars template
   let params = { seo: seo };
   // get our options
-  params.options = astra.getOptionCounts(pollOptions);
+  params.options = await astra.getOptionCounts(pollOptions);
   reply.view("/src/pages/index.hbs", params);
 });
 
 // A POST route to handle and react to form submissions
 fastify.post("/", async function (request, reply) {
   let params = { seo: seo };
-  // the request.body.color is posted with a form submission
-  let color = request.body.color;
-  // if it's not empty, let's try to find the color
-  if (color) {
-    // load our color data file
-    const colors = require("./src/colors.json");
-    // take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    // now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // found one!
-      // save a color history entry to astra
-      await astra.addOptionHistory({
-        name: color
-      });
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-        optionHistory: await astra.getOptionHistory(),
-      };
-    } else {
-      // try again.
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-        optionHistory: await astra.getOptionHistory(),
-      };
-    }
+  if(request.body.option){
+    params.picked = true;
+    await astra.incrementOption(request.body.option);
+  } else {
+    params.options = await astra.getOptionCounts(pollOptions);
   }
   reply.view("/src/pages/index.hbs", params);
 });
