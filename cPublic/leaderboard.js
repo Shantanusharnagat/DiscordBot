26717c448a5e8ecedeaad190a3438f90f91e8793
Glitch (insidious-pear-wildflower) <none>
2022-11-26 11:08:23 +0000

    👐💺 Checkpoint ./cPublic/leaderboard.js:48593061/1583

--- cPublic/leaderboard.js
+++ cPublic/leaderboard.js
@@ -5,14 +5,19 @@ module.exports.run = async(bot, message, args) =>{
 
     const user = require('../mongoDB/models/user')
     let allUsers = await user.find()
-    let leaderboard = `**__Leaderboard__ :**\n`
-
+    let leaderboard = ``
     for(let i=0;i<allUsers.length;i++){
         userInfo = bot.users.cache.find(user => user.id === allUsers[i].userid)
         leaderboard+=`*${userInfo.username} Points*: **${allUsers[i].points}**\n`
     }
+    embed
+          .setTitle("**Leaderboard**")
+          .setDescription(`${leaderboard}`)
+          .setThumbnail("https://img.freepik.com/premium-vector/realistic-golden-trophy-with-text-space_48799-1061.jpg?w=2000")
 
-    message.channel.send(leaderboard)
+    message.channel.send({embeds: [embed]})
+
+  
 
 }
 
