f74d9102c9c581946466b615e366e724d8c5bd3b
Glitch (insidious-pear-wildflower) <none>
2022-11-27 04:53:49 +0000

    ❄️🌞 Checkpoint ./cPublic/cash.js:48593061/191

--- cPublic/cash.js
+++ cPublic/cash.js
@@ -5,15 +5,17 @@ module.exports.run = async(bot, message, args) =>{
     const embed = new MessageEmbed()
   const user = require('../mongoDB/models/user')
   let curuser=await user.find({ "userid": message.author.id})
-  console.log(curuser)
+
     cash(message.author)
-  console.log(curuser.cash)
+  
+  console.log(curuser.userid)
+
   
 
     embed
         .setTitle(`**Paisa hi Paisa**`)
         .setColor("GOLD") 
-        .setDescription(`** Points*: **${curuser.cash}**\n`)
+        .setDescription(`** Cash **: **${curuser.user.cash}**\n`)
         .setThumbnail('https://static.toiimg.com/thumb/resizemode-4,width-1200,height-900,msid-87930581/87930581.jpg')
 
     message.channel.send({embeds: [embed]})
