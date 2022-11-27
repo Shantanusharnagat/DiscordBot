54d9c86bdff8ad678a0051e53cab4058601e78b0
Glitch (insidious-pear-wildflower) <none>
2022-11-27 11:21:17 +0000

    👑🐄 Checkpoint ./cPublic/mute.js:48593061/1867

---
+++ cPublic/mute.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } =  require("discord.js")
+
+module.exports.run = async(bot, message, args) =>{
+  
+    
+    
+}
+
+
+
+module.exports.help = {
+    
+    name:"mute",
+    aliases: "",
+    desc: "mutes people"
+}
+
