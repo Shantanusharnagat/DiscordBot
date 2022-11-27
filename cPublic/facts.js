fab7e4ede1287fb542df41c36670afee0df6a281
Glitch (insidious-pear-wildflower) <none>
2022-11-27 10:35:04 +0000

    🍃🦄 Checkpoint ./cPublic/facts.js:48593061/996

--- cPublic/facts.js
+++ cPublic/facts.js
@@ -5,14 +5,17 @@ const OneAI = require("oneai");
 module.exports.run = async(bot, message, args) =>{
     const embed = new MessageEmbed()
    
-    const oneai = new OneAI(process.env.ONEAI);
-const text = "Whether to power translation to document summarization, enterprises are increasing their investments in natural language processing (NLP) technologies. According to a 2021 survey from John Snow Labs and Gradient Flow, 60% of tech leaders indicated that their NLP budgets grew by at least 10% compared to 2020, while a third said that spending climbed by more than 30%";
+
+
+const oneai = new OneAI(process.env.ONEAI);
+const text = "I feel very bad about hurting Michelle the other day.";
 const pipeline = new oneai.Pipeline(
-    oneai.skills.summarize(),
+    oneai.skills.sentiments(),
 );
 
-pipeline.run(text).then(console.log);
-
+pipeline.run(text).then((res)=>{
+  console.log('RES: ', res.sentiments.value)
+});
     
     
     
