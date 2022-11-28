5ae592f4fb40392e7b8a15777ba5af3b32f0e066
Glitch (insidious-pear-wildflower) <none>
2022-11-28 21:30:26 +0000

    ☄️💍 Checkpoint ./cPublic/getgun.js:48593061/1058

--- cPublic/getgun.js
+++ cPublic/getgun.js
@@ -1,26 +1,14 @@
 const { MessageEmbed } =  require("discord.js")
 
-module.exports.run = async(bot, message, args) =>{
+module.exports.run = async(req, res, bot, message, args) =>{
     const embed = new MessageEmbed()
     
-    const getANS =async (req, res, next)=>{
-    try{
-        await (res.data)
-        res.status(200).json("PLot has been deleted")
-    }catch(err){
-        next(err)
-    }
-}
+    
+  
   
   
 
-    embed
-        .setTitle(`SUIIIIIII`)
-        .setColor("RED") 
-        .setDescription(``)
-        .setThumbnail('https://static.toiimg.com/thumb/resizemode-4,width-1200,height-900,msid-87930581/87930581.jpg')
-
-    message.channel.send({embeds: [embed]})
+    
     
     
 }
@@ -29,8 +17,8 @@ module.exports.run = async(bot, message, args) =>{
 
 module.exports.help = {
     
-    name:"ping",
-    aliases: "p",
+    name:"getgun",
+    aliases: "gg",
     desc: "Ping Status of the Bot to Discord Server"
 }
 
