3dbe26e4569920e80ce0ce18b73b63fa39640f53
Glitch (insidious-pear-wildflower) <none>
2022-12-05 12:10:10 +0000

    🐽👖 Checkpoint ./cPublic/gif.js:48593061/982

--- cPublic/gif.js
+++ cPublic/gif.js
@@ -2,14 +2,23 @@ const { MessageEmbed } =  require("discord.js")
 
 module.exports.run = async(bot, message, args) =>{
     const embed = new MessageEmbed()
-
+    
+   
+            let url = 'https://api.giphy.com/v1/gifs/search?api_key=Ml4YJpXHLpXCSldzAKQJ7Lmey3WN97RB&q=' + args;
+            fetch(url)
+                .then(response => response.json())
+                .then(content => {
+                    
+                     img=content.data[0].images.original.url
+                });    
 
 
     embed
-        .setTitle(`SUIIIIIII`)
+        .setTitle(`GIF`)
         .setColor("RED") 
-        .setDescription(`**🕵️‍♂️ Ping Response ${ping}ms**`)
+        .setDescription(``)
         .setThumbnail('https://static.toiimg.com/thumb/resizemode-4,width-1200,height-900,msid-87930581/87930581.jpg')
+        .setImage(img)
 
     message.channel.send({embeds: [embed]})
     
@@ -20,8 +29,8 @@ module.exports.run = async(bot, message, args) =>{
 
 module.exports.help = {
     
-    name:"ping",
-    aliases: "p",
+    name:"gif",
+    aliases: "g",
     desc: "Ping Status of the Bot to Discord Server"
 }
 
